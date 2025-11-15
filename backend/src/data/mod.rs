619c69f810f4bb5bbc1c00a8e03445b8e3063125
Ivan <pizzuhh@protonmail.com>
2025-11-15 15:22:37 +0200

    Add a way to request challanges

--- backend/src/data/mod.rs
+++ backend/src/data/mod.rs
@@ -1,5 +1,15 @@
 use uuid::Uuid;
 
+
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db_connection: sqlx::PgPool,
+    pub token: String,
+}
+
+
+
 #[derive(serde::Serialize, serde::Deserialize, sqlx::FromRow, Debug)]
 pub struct User {
     pub id: Uuid,
@@ -8,4 +18,24 @@ pub struct User {
     pub password_hash: Option<String>,
     pub is_admin: Option<bool>,
     pub points: Option<i32>
+}
+
+#[derive(serde::Serialize, serde::Deserialize, sqlx::Type, Debug)]
+#[sqlx(type_name="quest_state")]
+#[sqlx(rename_all="PascalCase")]
+pub enum Progress {
+    Verefied,
+    Pending,
+    Denied
+}
+
+#[derive(serde::Serialize, serde::Deserialize, sqlx::FromRow, Debug)]
+pub struct Quest {
+    id: Uuid,
+    progress: Option<Progress>,
+    name: String,
+    description: String,
+    required_points: i32,
+    points_received: i32,
+    user_id: Option<Uuid>
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,12 @@
 use std::env;
 
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 use dotenv::dotenv;
 use sqlx::{query_as, PgPool};
 
 
 mod data;
-
-async fn test() -> &'static str {
-    return "ok";
-}
-
-
-struct AppState {
-
-}
+mod handlers;
 
 #[tokio::main]
 async fn main() {
@@ -25,11 +17,19 @@ async fn main() {
 
     let db_connection = PgPool::connect(url.as_str()).await.unwrap();
 
+    let state = data::AppState {
+        db_connection: db_connection.clone(),
+        token: "tmp".to_string()
+    };
+
 
     let t: data::User = query_as!(data::User, "SELECT * FROM users;").fetch_one(&db_connection).await.unwrap();
     println!("{:?}", t);
 
-    let app = Router::new().route("/api", get(test));
+    let app = Router::new()
+                    .route("/challage/receive", get(handlers::request_challange))
+                    .route("/challange/send/{id}",post(handlers::send_challange))
+                    .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7564").await.unwrap();
 
