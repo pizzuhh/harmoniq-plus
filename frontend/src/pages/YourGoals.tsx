3a2eac40a0240353e9088615d3e65dd17765b9cc
pizzuhh <admin@pizzuhh.dev>
2025-12-13 15:18:48 +0200

    CSCSCSCSCSC

--- frontend/src/pages/YourGoals.tsx
+++ frontend/src/pages/YourGoals.tsx
@@ -385,13 +385,13 @@ const styles = {
     margin: 0,
     fontSize: '36px',
     fontWeight: 'bold',
-    color: '#fff',
+    color: '#000000ff',
     textShadow: '0 2px 10px rgba(0,0,0,0.2)',
   } as React.CSSProperties,
   subtitle: {
     margin: '8px 0 0',
     fontSize: '16px',
-    color: 'rgba(255,255,255,0.9)',
+    color: 'rgba(0, 0, 0, 0.9)',
   } as React.CSSProperties,
   createBtn: {
     padding: '12px 24px',
